d1eef55aa6f347e821d3d992cce7b98714ec002f
Priyank <Priyank@Priyank-PC>
2021-03-15 22:00:28 +0530

    5 Java Program to Compute Quotient and Remainder

---
+++ JavaBasics/src/JavaLogicalProgram/QuotientandRemainder.java
@@ -0,0 +1,18 @@
+package JavaLogicalProgram;
+
+public class QuotientandRemainder {
+
+	public static void main(String[] args) {
+		//5 Java Program to Compute Quotient and Remainder
+		
+		int devident  = 30;
+		int devide = 4;
+		int Q = devident/devide;
+		int R = devident%devide;
+		
+		System.out.println("Quotient is ="+ Q);
+		System.out.println("Remainder is ="+ R);
+
+	}
+
+}
