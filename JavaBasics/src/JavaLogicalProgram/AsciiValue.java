ee0cd1aa3d757ead0ee75c7f23f48b90f42f2b3e
Priyank <Priyank@Priyank-PC>
2021-03-15 21:56:06 +0530

    4 Java Program to Find ASCII Value of a character

---
+++ JavaBasics/src/JavaLogicalProgram/AsciiValue.java
@@ -0,0 +1,14 @@
+package JavaLogicalProgram;
+
+public class AsciiValue {
+
+	public static void main(String[] args) {
+		// 4 Java Program to Find ASCII Value of a character
+		
+		char c = 'A';
+		int ascii = (int)c;
+		System.out.println("ascii of given value is ="+ascii);
+		
+	}
+
+}
