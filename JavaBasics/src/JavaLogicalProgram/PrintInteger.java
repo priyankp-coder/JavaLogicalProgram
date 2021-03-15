1ce1212e655090f676b219a74c764be78f412f73
Priyank <Priyank@Priyank-PC>
2021-03-15 21:46:35 +0530

    Second Program

---
+++ JavaBasics/src/JavaLogicalProgram/PrintInteger.java
@@ -0,0 +1,16 @@
+package JavaLogicalProgram;
+
+import java.util.Scanner;
+
+public class PrintInteger {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner reader = new Scanner(System.in);
+		int num = reader.nextInt();
+		System.out.println("value entered by the user is ="+num);
+		
+
+	}
+
+}
