fba52068e0346085a801d635db1024b0ea4ca397
Priyank <Priyank@Priyank-PC>
2021-03-15 21:52:16 +0530

    3rd Program -- Java Program to Multiply two Floating Point Numbers.

---
+++ JavaBasics/src/JavaLogicalProgram/FloatMultiplication.java
@@ -0,0 +1,16 @@
+package JavaLogicalProgram;
+
+public class FloatMultiplication {
+
+	public static void main(String[] args) {
+		// 3 Java Program to Multiply two Floating Point Numbers
+		
+		float f1 = 2.25f;
+		float f2 = 3.25f;
+		
+		System.out.println("Multiplication of two floating number is = "+(f1*f2));
+		
+
+	}
+
+}
