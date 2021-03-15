807983a2ff939b3420496e0b5cafa1021b281488
Priyank <Priyank@Priyank-PC>
2021-03-15 21:33:23 +0530

    First Program of Addition of Two Number

---
+++ JavaBasics/src/JavaLogicalProgram/AddInteger.java
@@ -0,0 +1,13 @@
+package JavaLogicalProgram;
+
+public class AddInteger {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		System.out.println("Addition of two integer = "+(a+b));
+		
+	}
+
+}
