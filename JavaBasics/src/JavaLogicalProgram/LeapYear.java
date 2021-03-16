79c4f47cbe67b830737892ed163d7e2ca10caeb0
Priyank <Priyank@Priyank-PC>
2021-03-16 11:55:27 +0530

    10 Leap Year Program

---
+++ JavaBasics/src/JavaLogicalProgram/LeapYear.java
@@ -0,0 +1,20 @@
+package JavaLogicalProgram;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// Java Program to Check Leap Year
+		
+		//divisble by 4 for all century year == ending with 00
+		//Century year is leap year only when its perfectly divisble by 400
+		//1900 is not a leap year
+		//2012 is leap year
+		
+		int year = 1900;
+		boolean leap = false;
+		
+		
+
+	}
+
+}
