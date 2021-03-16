d54fa1e4416a2a502fed0632679f7038403787ed
Priyank <Priyank@Priyank-PC>
2021-03-16 09:48:48 +0530

    9 Java Program to Find the Largest Among Three Numbers

---
+++ JavaBasics/src/JavaLogicalProgram/LargestAmongThreeNumbers.java
@@ -0,0 +1,26 @@
+package JavaLogicalProgram;
+
+public class LargestAmongThreeNumbers {
+
+	public static void main(String[] args) {
+		// 9 Java Program to Find the Largest Among Three Numbers
+		
+		int a =600;
+		int b = 500;
+		int c = 300;
+		
+		if (a>b && a>c) {
+			
+			System.out.println("a is largest Number");
+		}
+		else if (b>c)
+		{
+			System.out.println("b is largest number");
+		}
+		else
+		{
+			System.out.println("c is largest number");
+		}
+	}
+
+}
