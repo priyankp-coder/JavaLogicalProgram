30e68794213c5b9bfeacdaf7e3177ae3bdd37aba
Priyank <Priyank@191.168.169.212>
2021-03-17 09:45:32 +0530

    15 Java Program to Generate Multiplication Table

---
+++ JavaBasics/src/JavaLogicalProgram/TableMultiplication.java
@@ -0,0 +1,19 @@
+package JavaLogicalProgram;
+
+public class TableMultiplication {
+
+	public static void main(String[] args) {
+		// 15 Java Program to Generate Multiplication Table
+		//2 x 1 = 2
+		//2 x 2 = 4
+		
+		int num = 18;
+		
+		for(int i = 1; i<=10;i++)
+		{
+			System.out.println(num+" x "+ i+ " = "+(num*i));
+		}
+
+	}
+
+}
