d5ed6f57fa7446e75530ed07db82f253e891e7ec
Priyank <Priyank@191.168.169.212>
2021-03-17 08:41:49 +0530

    13 Java Program to Calculate the Sum of Natural Numbers

---
+++ JavaBasics/src/JavaLogicalProgram/SumofNaturalNumbers.java
@@ -0,0 +1,19 @@
+package JavaLogicalProgram;
+
+public class SumofNaturalNumbers {
+
+	public static void main(String[] args) {
+		// 13 Java Program to Calculate the Sum of Natural Numbers
+		
+		int num = 100;
+		int sum =0;
+		
+		for(int i=1;i<=num;i++)
+		{
+			sum = sum+i;			
+		}
+		System.out.println("Sum is ="+ sum);
+
+	}
+
+}
