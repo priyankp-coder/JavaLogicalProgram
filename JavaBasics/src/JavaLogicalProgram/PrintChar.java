e7c1f43a9f56797ae365a8b3cb9eb6f5fedc60ab
Priyank <Priyank@191.168.169.212>
2021-03-17 10:16:52 +0530

    17 Java Program to Display Characters from A to Z using loop.

---
+++ JavaBasics/src/JavaLogicalProgram/PrintChar.java
@@ -0,0 +1,15 @@
+package JavaLogicalProgram;
+
+public class PrintChar {
+
+	public static void main(String[] args) {
+		// 17 Java Program to Display Characters from A to Z using loop.
+		
+		for(char c ='A';c<='Z';c++)
+		{
+			System.out.print(c+" ");
+		}
+
+	}
+
+}
